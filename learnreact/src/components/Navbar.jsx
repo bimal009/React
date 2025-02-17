070df0de2a73cb67731ea3ef91f09de4554be9fe
bimal009 <pandeybimal616@gmail.com>
2025-02-17 10:12:31 +0545

    first commit

---
+++ learnreact/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-black'>
+      hello worldhellllllllllllllllllllllllllllllllll
+    </div>
+  )
+}
+
+export default Navbar
